06cf51c348cfc920b641055df8d502287cf0d307
gitalek <sunny.os87@rambler.ru>
2018-04-27 18:32:40 +1000

    add views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,43 @@
-# Users controller class
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'instarello',
+        avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUz0lGPYv9BTmms-UuRmAKyxRb5w25z62kbpIfr4ZeHh7ptY_7'
+      ),
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'aristofun',
+      )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = 'Привет, Мишаня!'
+    # @user = User.new(
+    #   name: 'Vadim',
+    #   username: 'instarello'
+    # )
+    @user = User.new(
+      name: 'Vadim',
+      username: 'instarello',
+      avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUz0lGPYv9BTmms-UuRmAKyxRb5w25z62kbpIfr4ZeHh7ptY_7'
+    )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'В чём смысл жизни?',
+                   created_at: Date.parse('28.03.2016'))
+    ]
+
+    @new_question = Question.new
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def user_avatar(user)
+    return asset_path 'avatar.jpg' unless user.avatar_url.present?
+    user.avatar_url
+  end
 end
